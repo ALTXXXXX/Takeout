a33b664ea1c10b22f073702645e86f8dcc79533d
ALTXXXXX <1078369700@qq.com>
2025-07-24 16:10:48 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -8,4 +11,8 @@ public interface DishMapper {
     //根据分类的id查询菜品数量
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
+
+    //添加菜品
+    @AutoFill(value = OperationType.INSERT)
+    void add(Dish dish);
 }
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,8 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+
+public interface DishService {
+   //添加菜品
+    void saveWithFlavor(DishDTO dishDTO);
+}
